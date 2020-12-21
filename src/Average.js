27b30712629a036dfa341123f927344352029d06
JunIl-git <64887129+JunIl-git@users.noreply.github.com>
2020-12-21 22:51:51 +0900

    Average

--- src/Average.js
+++ src/Average.js
@@ -1,36 +1,40 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
-const Average = () => {
-    const [number, setNumber] = useState('');
-    const [list, setList] = useState([]);
+const Average = () => {
+    const [number, setNumber] = useState('');
+    const [list, setList] = useState([]);
 
-    const handleChange = e => {
-        setNumber(e.target.value);
-    }
+    const handleChange = e => {
+        setNumber(e.target.value);
+    }
 
-    const handleClick = () => {
-        const nextList = list.concat(parseInt(number));
-        console.log(nextList);
-        setList(nextList);
-        setNumber('');
-    }
+    const handleClick = () => {
+        const nextList = list.concat(parseInt(number));
+        console.log(nextList);
+        setList(nextList);
+        setNumber('');
+    }
 
-    const getAverage = lists => {
-        if(list.length === 0){return 0;}
-        const sum = lists.reduce(( a , b )=> a + b )
-        return sum / lists.length;
-    }
-    return(
-        <>
-            <input value={number} onChange={handleChange}/>
-            <button onClick={handleClick}></button>
-            <ul>{list.map((value,index)=>{
-                return <li key={index}>{value}</li>
-            })}</ul>
+    const getAverage = lists => {
+        console.log("평균값 계산")
+        if(list.length === 0){return 0;}
+        const sum = lists.reduce(( a , b )=> a + b )
+        return sum / lists.length;
+    }
 
-            <h1>{getAverage(list)}</h1>
-        </>
-    )
+    const avg = useMemo(()=> getAverage(list),[list]);
+
+    return(
+        <>
+            <input value={number} onChange={handleChange}/>
+            <button onClick={handleClick}></button>
+            <ul>{list.map((value,index)=>{
+                return <li key={index}>{value}</li>
+            })}</ul>
+
+            <h1>{avg}</h1>
+        </>
+    )
 }
 
-export default Average;
+export default Average;
